b9a96b25ae33086399bc36f9dd59b41124894a8d
janderdeoliveira <janderfrancisco@hotmail.com>
2020-07-29 23:37:09 -0300

    filtragem de dados

--- src/components/Tasks/ListTasks.vue
+++ src/components/Tasks/ListTasks.vue
@@ -1,6 +1,22 @@
 <template>
     <div>
         <h2>{{ title }}</h2>
+        <div class="row">
+
+        <div class="col">
+            <form action="form form-inline" @submit.prevent="onSubmit()">
+                <input type="text" name="" placeholder="Nome da tarafa" class="form-control" v-model="task.name">
+                <button type="submit" class="btn btn-primary">Enviar</button>
+            </form>
+        </div>
+        <div class="col">
+            <form action="form form-inline" @submit.prevent="onSubmit()">
+                <input type="text" name="" placeholder="Nome da tarafa" class="form-control" v-model="filter">
+               
+            </form>
+
+            </div>
+        </div>
         <table class="table table-dark">
             <thead> 
                 <tr>
@@ -10,13 +26,13 @@
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>asdfas</td>
+                <tr v-for="(task, index) in filteredTask"  :key="index">
+                    <td>{{ task.id }}</td>
+                    <td>{{ task.name }}</td>
                     <td>
-                        <a href="">Editar</a>
-                        <a href="">Deletar</a>
-                        <a href="">Deletar</a>
+                        <a href="#" @click.prevent="editTask(index)" class="btn btn-info">Editar</a>
+                        <a href="#"@click.prevent="deleteTask(index)"  class="btn btn-danger">Deletar</a>
+                      
                     </td>
                 </tr>
             </tbody>
@@ -29,11 +45,70 @@ export default {
     data(){
         return{
             title: 'Lista de Tarefas',
-            tasks: []
+            tasks: [],
+            task:{
+                id: '',
+                name: ''
+            },
+            updating: false,
+            updateIndex: '',
+            filter: ''
         } 
+    },
+    methods:{
+        onSubmit (){
+            if ( this.updating){
+                this.update()
+                return;
+            }
+
+            this.saveTask()
+        },
+        saveTask(){
+            let idTask = this.tasks.length + 1
+            this.task.id = idTask
+
+            this.tasks.push(this.task)
+            this.clearForm() 
+        },
+        editTask (index){
+            this.updating = true
+            this.updateIndex = index
+            this.task = this.tasks[index]
+        },
+        update(){
+            this.tasks[this.updateIndex] = this.task
+            this.updating = false
+            this.clearForm()
+        },
+        clearForm(){
+            this.task =  {
+                id: '',
+                name: ''
+            } 
+        },
+        deleteTask (index){
+            this.tasks.splice(index, 1)
+        }
+    },
+    computed:{
+        filteredTask (){
+            if (this.filter === '')
+                return this.tasks
+            
+            let vm = this
+            return this.tasks.filter(task => {
+                return task.name.toLowerCase().indexOf(vm.filter.toLowerCase()) > -1
+            })
+            
+        }
     }
 }
 </script>
-<style >
+<style scoped>
+form{
+    margin: 20px 0
+}
 
+ 
 </style>
