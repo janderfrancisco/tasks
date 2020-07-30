a0337521dc9fb34ff292888fb763f963086683d2
janderdeoliveira <janderfrancisco@hotmail.com>
2020-07-29 22:58:30 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app" class="container">
+      <list-tasks></list-tasks>
+  </div>
+</template>
+
+<script>
+import ListTasks from './components/Tasks/ListTasks'
+
+export default {
+  name: 'app',
+  components:{
+    ListTasks
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+ 
---
+++ src/components/Tasks/ListTasks.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <h2>{{ title }}</h2>
+        <table class="table table-dark">
+            <thead> 
+                <tr>
+                    <th>Id.</th>
+                    <th>Nome</th>
+                    <th width="150px">Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>asdfas</td>
+                    <td>
+                        <a href="">Editar</a>
+                        <a href="">Deletar</a>
+                        <a href="">Deletar</a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            title: 'Lista de Tarefas',
+            tasks: []
+        } 
+    }
+}
+</script>
+<style >
+
+</style>
